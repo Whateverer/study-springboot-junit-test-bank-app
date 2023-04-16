6c6d4e05dc6253e1636b2549312ca9087aae98e7
yujin <hsjes777@gmail.com>
2023-04-16 20:18:32 +0900

    본인계좌 목록보기 컨트롤러 만들기

--- src/main/java/shop/mtcoding/bank/web/AccountController.java
+++ src/main/java/shop/mtcoding/bank/web/AccountController.java
@@ -5,10 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import shop.mtcoding.bank.config.auth.LoginUser;
 import shop.mtcoding.bank.dto.ResponseDto;
 import shop.mtcoding.bank.dto.account.AccountReqDto;
@@ -32,5 +29,16 @@ public class AccountController {
                                          BindingResult bindingResult, @AuthenticationPrincipal LoginUser loginUser) {
         AccountSaveRespDto accountSaveRespDto = accountService.계좌등록(accountSaveReqDto, loginUser.getUser().getId());
         return new ResponseEntity<>(new ResponseDto<>(1, "계좌등록 성공", accountSaveRespDto), HttpStatus.CREATED);
+    }
+
+    // 인증이 필요하고, account 테이블에 1번 row를 주세요!!
+    // 인증이 필요하고, account 테이블 데이터 다 주세요!!
+    // 권한 처리 때문에 선호하지 않는다.
+
+    // 인증이 필요하고, account 테이블에 login한 유저의 계좌만 주세요.
+    @GetMapping("/s/account/login-user")
+    public ResponseEntity<?> findUserAccount(@AuthenticationPrincipal LoginUser loginUser) {
+        AccountListRespDto accountRespDto = accountService.계좌목록보기_유저별(loginUser.getUser().getId());
+        return new ResponseEntity<>(new ResponseDto<>(1, "", null), HttpStatus.OK);
     }
 }
