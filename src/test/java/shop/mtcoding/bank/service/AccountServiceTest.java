2c5b6917be7a58b8d97382ab94b7bebd54822f3a
장유진 <jin94@exotech.kr>
2023-04-18 13:17:21 +0900

    계좌입금 서비스 테스트 만들기(스스로)

--- src/test/java/shop/mtcoding/bank/service/AccountServiceTest.java
+++ src/test/java/shop/mtcoding/bank/service/AccountServiceTest.java
@@ -2,6 +2,7 @@ package shop.mtcoding.bank.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,12 +12,17 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import shop.mtcoding.bank.config.dummy.DummyObject;
 import shop.mtcoding.bank.domain.account.Account;
 import shop.mtcoding.bank.domain.account.AccountRepository;
+import shop.mtcoding.bank.domain.transaction.Transaction;
+import shop.mtcoding.bank.domain.transaction.TransactionEnum;
+import shop.mtcoding.bank.domain.transaction.TransactionRepository;
 import shop.mtcoding.bank.domain.user.User;
 import shop.mtcoding.bank.domain.user.UserRepository;
 import shop.mtcoding.bank.dto.account.AccountReqDto;
 import shop.mtcoding.bank.dto.account.AccountRespDto;
 import shop.mtcoding.bank.handler.ex.CustomApiException;
+import shop.mtcoding.bank.util.CustomDateUtil;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -25,6 +31,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static shop.mtcoding.bank.dto.account.AccountReqDto.*;
 import static shop.mtcoding.bank.dto.account.AccountRespDto.*;
+import static shop.mtcoding.bank.service.AccountService.*;
 
 @ExtendWith(MockitoExtension.class) // 전체 환경이 필요없기 때문에 mockito로 생성
 class AccountServiceTest extends DummyObject {
@@ -36,6 +43,9 @@ class AccountServiceTest extends DummyObject {
 
     @Mock
     private AccountRepository accountRepository;
+
+    @Mock
+    private TransactionRepository transactionRepository;
 
     @Spy // 진짜 객체를 InjectMocks에 주입한다.
     private ObjectMapper om;
@@ -96,5 +106,40 @@ class AccountServiceTest extends DummyObject {
 
         // then
         assertThrows(CustomApiException.class, () -> accountService.계좌삭제(number, userId));
+    }
+
+    @Test
+    void 계좌입금_test() {
+        // given
+        AccountDepositReqDto accountDepositReqDto = new AccountDepositReqDto();
+        accountDepositReqDto.setAmount(100L);
+        accountDepositReqDto.setNumber(1111L);
+        accountDepositReqDto.setGubun(TransactionEnum.DEPOSIT.getValue());
+
+        // stub 1
+        Account account = newMockAccount(1L, 1111L, 1000L, newMockUser(1L, "ssar", "쌀"));
+        when(accountRepository.findByNumber(any())).thenReturn(Optional.of(account));
+
+        account.deposit(accountDepositReqDto.getAmount());
+
+        // stub 2
+        Transaction transaction = Transaction.builder()
+                .depositAccount(account)
+                .withdrawAccount(null)
+                .depositAccountBalance(account.getBalance())
+                .withdrawAccountBalance(null)
+                .amount(accountDepositReqDto.getAmount())
+                .gubun(TransactionEnum.DEPOSIT)
+                .sender("ATM")
+                .receiver(accountDepositReqDto.getNumber()+"")
+                .tel(accountDepositReqDto.getTel())
+                .createdAt(LocalDateTime.now())
+                .build();
+        when(transactionRepository.save(any())).thenReturn(transaction);
+        // when
+        AccountDepositRespDto accountDepositRespDto = accountService.계좌입금(accountDepositReqDto);
+
+        // then
+        assertThat(accountDepositRespDto.getTransaction().getDepositAccountBalance()).isEqualTo(1100L);
     }
 }
