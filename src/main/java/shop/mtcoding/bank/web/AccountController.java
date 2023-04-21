8d6be542c65746165b315f7b9e14cc7d0b60f312
장유진 <jin94@exotech.kr>
2023-04-21 12:43:45 +0900

    계좌상세보기 서비스, 컨트롤러 생성 및 테스트

--- src/main/java/shop/mtcoding/bank/web/AccountController.java
+++ src/main/java/shop/mtcoding/bank/web/AccountController.java
@@ -67,4 +67,12 @@ public class AccountController {
         AccountTransferRespDto accountTransferRespDto = accountService.계좌이체(accountTransferReqDto, loginUser.getUser().getId());
         return new ResponseEntity<>(new ResponseDto<>(1, "계좌 이체 완료", accountTransferRespDto), HttpStatus.CREATED);
     }
+
+    @GetMapping("/s/account/{number}")
+    public ResponseEntity<?> findDetailAccount(@PathVariable Long number,
+                                               @RequestParam(value = "page", defaultValue = "0") Integer page,
+                                               @AuthenticationPrincipal LoginUser loginUser) {
+        AccountDetailRespDto accountDetailRespDto = accountService.계좌상세보기(number, loginUser.getUser().getId(), page);
+        return new ResponseEntity<>(new ResponseDto<>(1, "계좌상세보기 성공", accountDetailRespDto), HttpStatus.OK);
+    }
 }
